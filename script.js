b267733060af2588eeaabe238add505c2ea74991
pietroTP <pietro.grandi.3d@gmail.com>
2015-04-23 17:38:10 +0200

    fixes for mobile visualization

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
-var theMaterial, theLight, theLight2, cubeNumber = 14, elem, cubes = [], cubeSize = 1;
-
-
+/* global THREE:true, $:true */
 function onStart() {
   'use strict';
 
   var
-    theContainer, theRenderer, theScene, theCamera, theGeometry,
-    theColor = 0x60A1E1, //60A1E1, //0x3B5CA9,
+    onResizeEnd, theGeometry, theMaterial, theLight, theLight2, elem,
+    cubes = [],
+    theColor = 0x60A1E1,
     transition = 0.002,
-    body = $('body'), h = body.height(), w = body.width();
-  //h = $(document).height(), w = $(document).width();
+    body = $('body'), h = body.height(), w = body.width(),
+    theContainer = $("#animation"),
+    theRenderer = new THREE.WebGLRenderer({alpha: true}),
+    theScene = new THREE.Scene(),
+    theCamera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 
-  theContainer = $("#animation");
-  theRenderer = new THREE.WebGLRenderer({alpha: true});
-  console.log(w, h);
   theRenderer.setSize(w, h);
-  theScene = new THREE.Scene();
-  theCamera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
   theCamera.position.set(0, 0, 10);
   theContainer.replaceWith(theRenderer.domElement);
 
-  theGeometry = new THREE.BoxGeometry(2, 2, 2);
   theMaterial = new THREE.MeshLambertMaterial({
     color: theColor
   });
 
-  $(window).resize(function (e) {
-    var
-      h = body.height(),
-      w = body.width();
+  $(window).resize(function () {
+    clearTimeout(onResizeEnd);
+    onResizeEnd = setTimeout(function () {
+      var
+        h = body.height(),
+        w = body.width();
 
-    theRenderer.setSize(w,h);
-    theCamera.aspect = w / h;
-    theCamera.updateProjectionMatrix();
-
-    //$('#myForm').width(w / k).css('left', w / 2 - (w / k / 2));
+      theRenderer.setSize(w, h);
+      theCamera.aspect = w / h;
+      theCamera.updateProjectionMatrix();
+    }, 400);
   });
 
-  function reSizeScene() {
+  function buildScene(cubeNumber, cubeSize) {
+    cubes = [];
+    theGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+
     for (var i = 0; i < cubeNumber; i += 1) {
       for (var k = 0; k < cubeNumber; k += 1) {
-        elem = cubes[(10 * i) + k];
-        elem.position.set(-cubeNumber + (cubeSize * 2 * k), -cubeNumber + (cubeSize * 2 * i), 0);
+        elem = new THREE.Mesh(theGeometry, theMaterial);
+        cubes.push(elem);
+        elem.position.set(-(cubeNumber * cubeSize / 2) + (cubeSize * k), -(cubeNumber * cubeSize / 2) + (cubeSize * i), 0);
+        theScene.add(elem);
       }
     }
+
   }
 
-  $(window).resize();
-  $('iframe').fadeIn();
-
-  for (var i = 0; i < cubeNumber; i += 1) {
-    for (var k = 0; k < cubeNumber; k += 1) {
-      elem = new THREE.Mesh(theGeometry, theMaterial);
-      cubes.push(elem);
-      elem.position.set(-cubeNumber + (cubeSize * 2 * k), -cubeNumber + (cubeSize * 2 * i), 0);
-      theScene.add(elem);
-    }
-  }
+  buildScene(Math.round(w / 16), w / 600);
 
   theLight = new THREE.DirectionalLight(0xD2D2D2, 1);
   theLight.position.set(20, 20, 20);
@@ -70,14 +63,14 @@ function onStart() {
 
   setInterval(function () {
     transition = -transition;
-  }, 3000);
+  }, 4000);
 
   function animate() {
     theRenderer.render(theScene, theCamera);
 
     $(cubes).map(function (index, cube) {
-      cube.rotation.y += 0.006;
-      cube.rotation.x += 0.006;
+      cube.rotation.y += 0.004;
+      cube.rotation.x += 0.004;
       cube.position.x += transition;
       cube.position.x += transition;
     });
